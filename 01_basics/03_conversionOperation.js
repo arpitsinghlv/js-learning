c233001d347481caaecc913a20c81398e4678cb8
Arpit Singh <arpitsinghlv@gmail.com>
2025-07-16 12:27:32 +0000

    Datatype Conversion

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,28 +1,67 @@
-let score = "33abc"
+// let score = "33abc"
 
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 
-let valueInNumber = Number(score);
-console.log(typeof valueInNumber);
-console.log(valueInNumber); // NaN - not a number
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber); // NaN - not a number
 
 
-// "33" => 33
-// "33abc" => NaN
-// true => 1; false => 0
+// // "33" => 33
+// // "33abc" => NaN
+// // true => 1; false => 0
 
-let isLoggedIn = 1
+// let isLoggedIn = 1
 
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
 
-// 1 => true; 0 => false:
-// "" => false;
-// "arpit" => true
+// // 1 => true; 0 => false:
+// // "" => false;
+// // "arpit" => true
 
-let someNumber = 3
+// let someNumber = 3
 
-let stringNumber = String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber);
+// let stringNumber = String(someNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber);
+
+// // ************** Operations ***************
+
+// let value = 3
+// let negValue = -value
+// console.log(negValue)
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/3);
+// console.log(2%3);
+
+// let str1 = "hello"
+// let str2 = " arpit"
+// let str3 = str1+str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1+2+"2");
+
+console.log(+true)
+console.log(+"")
+
+let num1, num2, num3 = 2 + 2
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+ 
+
+
+
+
+
+
